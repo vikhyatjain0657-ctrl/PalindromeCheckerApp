bf99a6dfc70db1fdf438c249497570756fa5a8b9
Vikhyat_jain <vikhyatjain0657@gmail.com>
2026-02-27 09:01:11 +0530

    UC8: Linked List Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,18 +1,19 @@
+import java.util.LinkedList;
 import java.util.Scanner;
-
 /**MAIN CLASS UseCaselPalindromeApp
 
-Use Case 4: Character Array Based Validation
+Use Case 8: Linked L ISt Based Patindrone Checker
 
  Description:
 
- This class validates a palindrome by converting the string into a character array and comparing characters using the two-pointer technique.
- At this stage, the application:
- Converts string to char array
- Uses start and end pointers
- Compares characters efficiently
- Displays the result
- This reduces extra memory usage.
+ This class cnecks whether a strzng is a pat Indrome
+ Sing a L InkedL2st.
+ haracters are added to the ust and then compared
+ y removing elements fron both ends:
+ - removeFirst()
+ - removeLast()
+ Thzs demonstrates how LankedLzst supports
+ ouble-ended operations for symmetric validation.
 
 @author Vikhyat
 @version 1.0
@@ -26,24 +27,29 @@ public class PalindromeCheckerApp {
      *
      * @param args Command-line arguments
      */
-    public static void main(String[] args){{
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter Text: ");
-        String input = in.next();
-        char[] chars = input.toCharArray();
-        int start = 0;
-        int end = chars.length - 1;
-        boolean isPalindrome = true;
-
-        while (start < end) {
-            if (chars[start] != chars[end]) {
-                isPalindrome = false;
-                break;
+    public static void main(String[] args){
+            Scanner in = new Scanner(System.in);
+            System.out.print("Enter Text: ");
+            String input = in.next();
+            LinkedList<Character> list = new LinkedList<>();
+            for (char c : input.toCharArray()) {
+                list.add(c);
             }
-            start++;
-            end--;
-        }
-        System.out.println("Is Palindrome?: " + isPalindrome);
-    }
+
+            boolean isPalindrome = true;
+
+            while (list.size() > 1) {
+                char first = list.removeFirst();
+                char last = list.removeLast();
+
+                if (first != last) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            System.out.println("Input : " + input);
+            System.out.println("Is Palindrome? : " + isPalindrome);
+            System.out.println("Is Palindrome?: " + isPalindrome);
     }
 }
