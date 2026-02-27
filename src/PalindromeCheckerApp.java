1ed869b185365714076f0cafacf2af1ce8117925
Vikhyat_jain <vikhyatjain0657@gmail.com>
2026-02-27 09:41:50 +0530

    Case-Insensitive & Space-Ignored Palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 /**MAIN CLASS UseCaselPalindromeApp
 
-Use Case 4: Character Array Based Validation
-
+Use Case 10: Case-Insensitive & Space-Ignored Palindrome
  Description:
 
- This class validates a palindrome by converting the string into a character array and comparing characters using the two-pointer technique.
- At this stage, the application:
- Converts string to char array
- Uses start and end pointers
- Compares characters efficiently
- Displays the result
- This reduces extra memory usage.
+ This class valldates a pahndrome uszng recursion.
+ Characters are compared from the outer positions
+ mcnng inward using recurnve calls.
+ The recurszon stops when
+ - All characters are matched,
+ - A nsratch is found.
+ This use case demonstrates divide-and-conquer
+ logic using method recursxon.
 
 @author Vikhyat
 @version 1.0
@@ -26,24 +26,24 @@ public class PalindromeCheckerApp {
      *
      * @param args Command-line arguments
      */
-    public static void main(String[] args){{
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter Text: ");
         String input = in.next();
-        char[] chars = input.toCharArray();
-        int start = 0;
-        int end = chars.length - 1;
+        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
         boolean isPalindrome = true;
 
-        while (start < end) {
-            if (chars[start] != chars[end]) {
+        for (int i = 0; i < normalized.length() / 2; i++) {
+            if (normalized.charAt(i) !=
+                    normalized.charAt(normalized.length() - 1 - i)) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
-        System.out.println("Is Palindrome?: " + isPalindrome);
-    }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
+
